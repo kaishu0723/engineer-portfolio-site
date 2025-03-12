c630962f024dd8691373581f15d8170742a91e98
kaishu <m.kaishu0723@gmailcom>
2025-03-12 11:06:22 +0900

    image responsive

--- src/app/components/introduse.tsx
+++ src/app/components/introduse.tsx
@@ -11,15 +11,6 @@ const introduse = () => {
         <div id="home" className="w-[100%] min-h-[640px] flex mt-10">
             <div className="trandition-all duration-300 lg:w-[50%] bg-black text-white p-10 w-[100%]">
                 <h2 className="text-6xl py-6 gText">Kaishu<br />Matsuo</h2>
-                {/* <p>
-                    こんにちは！ 私は松尾海秀です。<br />
-                    福岡県久留米市の久留米工業大学で情報工学を専攻している四年生です。<br />
-                    主にCGやフロントエンド開発を得意としていますが、
-                    他にもバックエンドやデータベース、AIも学んでいます。<br />
-                    これまで大学の講義で学んだことを生かして、3DCG作品やウェブサイト製作、
-                    「作品１」などに取り組んできました。<br />
-                    このページではその中のいくつかのプロジェクトを紹介しています。
-                </p> */}
                 <div className="sm:flex sm:space-x-4">
                     <section className="my-4 sm:w-1/2">
                         <h4 className="text-2xl gText"><AssignmentIndIcon style={{ color: '#FF64FF' }} className="relative top-[-3px] mr-2" />About me</h4>
@@ -52,7 +43,7 @@ const introduse = () => {
                     </div>
                 </div>
             </div>
-            <div className="trandition-all duration-300 lg:w-[50%] relative overflow-x-hidden w-[0%]">
+            <div className="trandition-all duration-300 lg:w-[50%] lg:h-auto lg:relative lg:right-0 lg:top-0 lg:rounded-none overflow-x-hidden absolute right-4 top-[90px] w-[200px] h-[200px] rounded-full">
                 <Image
                     src={"/images/tempIntro.png"}
                     alt="bg"
