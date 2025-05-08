a0fa17253c8c98ed4ef4af81c67b84f06efe877d
e224237 <e224237mk@kurume-it.ac.jp>
2025-05-08 12:17:45 +0900

    fix products and skills commit

--- src/app/components/products.tsx
+++ src/app/components/products.tsx
@@ -13,27 +13,13 @@ const products = () => {
         1000: 2,
         680: 1,
     };
-    const products = [//I'll edit this
-        {
-            title: "Simple Task Note",
-            description: "Next.jsフレームワークを利用した素早く記述できる。",
-            media: "/images/to-do-list.png",
-            link: "https://github.com/kaishu0723/to-do-list",
-            tools: ["TypeScript", "Next.js"],
-        },
+    const products = [
         {
             title: "Character Counter",
             description: "文字数をカウントするサービスです。",
             media: "/images/character-counter.png",
             link: "https://character-counter-seven.vercel.app/",
             tools: ["TypeScript", "Next.js", "Tailwind CSS"],
-        },
-        {
-            title: "Schedule",
-            description: "友人間で空いてる日を共有するための簡易プラットフォーム",
-            media: "/images/schedule.png",
-            link: "https://schedule-sepia.vercel.app/",
-            tools: ["TypeScript", "Next.js", "Tailwind CSS", "FireBase"],
         },
         {
             title: "New Project",
@@ -44,7 +30,7 @@ const products = () => {
         },
     ]
     return (
-        <div className="my-[100px]">
+        <div className="py-[100px] bg-gray-100">
             <h2 className="text-6xl text-center gText mb-12">Products</h2>
             <Masonry
                 breakpointCols={breakpointColumnsObj}
@@ -54,9 +40,9 @@ const products = () => {
             >
                 {
                     products.map((product, index) => (
-                        <div key={index} className="my-4 w-auto h-auto min-w-[320px] min-h-[350px] rounded-md shadow-lg relative overflow-hidden bg-white bottom-0 hover:bottom-4 trandition-all duration-300">
+                        <div key={index} className="my-4 w-auto min-w-[320px] h-[180px] rounded-md shadow-lg relative overflow-hidden bg-black bottom-0 hover:bottom-4 hover:h-[280px] trandition-all duration-300">
                             <a href={product.link}>
-                                <div className="w-[100%] h-[120px] relative">
+                                <div className="w-[100%] h-[180px] relative">
                                     <Image
                                         src={product.media}
                                         alt="media"
@@ -64,13 +50,10 @@ const products = () => {
                                         className="object-cover"
                                     />
                                 </div>
-                                <h3 className="text-2xl p-4 font-medium text-black">{product.title}</h3>
-                                <p className="text-gray-400 px-4">{product.description}</p>
-                                <ul className="flex flex-wrap absolute bottom-4 w-4/5">
-                                    {product.tools.map((tool, id) => (
-                                        <li key={id} className="bg-gray-100 rounded-md shadow-sm p-2 ml-2 mt-2">{tool}</li>
-                                    ))}
-                                </ul>
+                                <span className="block p-4 text-white w-[100%] h-auto">
+                                    <h3 className="text-2xl">{product.title}</h3>
+                                    <p>{product.description}</p>
+                                </span>
                             </a>
                         </div>
                     ))
